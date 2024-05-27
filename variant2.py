1c4f3ceddd16503232eff712f30825eff67b7d99
zlatkov <alexander.z.zlatkov@gmail.com>
2024-05-27 20:31:09 +0300

    Algo 3

--- variant2.py
+++ variant2.py
@@ -95,22 +95,9 @@ class AdjacencyGraph:
 
 
 class FragmentOverlap:
-    def __init__(self, fragment, overlapping_characters):
+    def __init__(self, fragment, overlapping_characters_count):
         self.fragment = fragment
-        self.overlapping_characters = overlapping_characters
-
-
-def build_prefix(end_node):
-    reversed_prefix = ''
-    node = end_node
-    while node and node.parent:
-        parent_node = node.parent
-        for ch, child_node in parent_node.children.items():
-            if child_node == node:
-                reversed_prefix += ch
-                node = parent_node
-                break
-    return reversed_prefix[::-1]
+        self.overlapping_characters_count = overlapping_characters_count
 
 
 def dump_branch_words(node):
@@ -152,10 +139,10 @@ def search_overlaps(trie, fragment, overlap_limit):
         overlapping_end_node = trie.search(suffix)
         if overlapping_end_node:
             branch_words = dump_branch_words(overlapping_end_node)
-            overlapping_characters = calculate_node_depth(overlapping_end_node)
+            overlapping_characters_count = calculate_node_depth(overlapping_end_node)
 
             for branch_word in branch_words:
-                overlaps.append(FragmentOverlap(suffix + branch_word, overlapping_characters))
+                overlaps.append(FragmentOverlap(suffix + branch_word, overlapping_characters_count))
         break
 
     return overlaps
@@ -166,13 +153,13 @@ def build_adjacency_graph(trie, fragments, overlap_limit):
     for fragment in fragments:
         overlaps = search_overlaps(trie, fragment, overlap_limit)
         for overlap in overlaps:
-            adjacency_graph.add_edge(fragment, overlap.fragment, overlap.overlapping_characters)
+            adjacency_graph.add_edge(fragment, overlap.fragment, overlap.overlapping_characters_count)
 
     return adjacency_graph
 
 
 if __name__ == '__main__':
-    FRAGMENTS = ['AABABBA', 'ABABBAC', 'ABABBAG', 'BABBACE', 'ABBACET']
+    FRAGMENTS = ['AACACCA', 'TCACCAG', 'ACACCAT', 'CACCATG', 'ACCATGA']
     OVERLAP_LIMIT = 4
     trie = build_trie(FRAGMENTS)
     adjacency_graph = build_adjacency_graph(trie, FRAGMENTS, OVERLAP_LIMIT)
