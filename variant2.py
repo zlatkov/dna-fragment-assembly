051a37dc03bc4834e9b9543bdee5fb5e987fe55d
zlatkov <alexander.z.zlatkov@gmail.com>
2024-05-27 09:06:53 +0300

    implemented algo 3

--- variant2.py
+++ variant2.py
@@ -38,11 +38,60 @@ class AdjacencyGraphEdge:
 class AdjacencyGraph:
     def __init__(self):
         self.edges = {}
+        self.in_degree = {}
+        self.out_degree = {}
 
     def add_edge(self, from_node, to_node, weight):
         if from_node not in self.edges:
             self.edges[from_node] = []
         self.edges[from_node].append(AdjacencyGraphEdge(to_node, weight))
+
+        if from_node not in self.out_degree:
+            self.out_degree[from_node] = 0
+        if to_node not in self.in_degree:
+            self.in_degree[to_node] = 0
+
+        self.out_degree[from_node] += 1
+        self.in_degree[to_node] += 1
+
+    def get_longest_path(self):
+        def dfs(node, current_path, current_length, visited, stack):
+            if node in stack:  # Cycle detected
+                return [], 0
+
+            if node in visited:
+                return visited[node]
+
+            max_path = current_path.copy()
+            max_length = current_length
+
+            stack.add(node)
+
+            for edge in self.edges.get(node, []):
+                new_path = current_path + [edge.to]
+                new_length = current_length + edge.weight
+                result_path, result_length = dfs(edge.to, new_path, new_length, visited, stack)
+
+                if result_length > max_length:
+                    max_path = result_path
+                    max_length = result_length
+
+            stack.remove(node)
+            visited[node] = (max_path, max_length)
+            return max_path, max_length
+
+        visited = {}
+        longest_paths = []
+        for node in self.edges.keys():
+            if self.in_degree.get(node, 0) == 0:
+                stack = set()
+                path, length = dfs(node, [node], 0, visited, stack)
+                longest_paths.append((path, length))
+
+        longest_paths.sort(key=lambda x: x[1], reverse=True)
+        if len(longest_paths) > 0:
+            return longest_paths[0]
+        return []
 
 
 class FragmentOverlap:
@@ -114,7 +163,6 @@ def search_overlaps(trie, fragment, overlap_limit):
 
 def build_adjacency_graph(trie, fragments, overlap_limit):
     adjacency_graph = AdjacencyGraph()
-    #pdb.set_trace()
     for fragment in fragments:
         overlaps = search_overlaps(trie, fragment, overlap_limit)
         for overlap in overlaps:
@@ -124,8 +172,8 @@ def build_adjacency_graph(trie, fragments, overlap_limit):
 
 
 if __name__ == '__main__':
-    FRAGMENTS = ['AABABBA', 'ABABBAC', 'ABABBAG']
+    FRAGMENTS = ['AABABBA', 'ABABBAC', 'ABABBAG', 'BABBACE', 'ABBACET']
     OVERLAP_LIMIT = 4
     trie = build_trie(FRAGMENTS)
     adjacency_graph = build_adjacency_graph(trie, FRAGMENTS, OVERLAP_LIMIT)
-    pdb.set_trace()
+    print(adjacency_graph.get_longest_path())
